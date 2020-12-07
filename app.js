776b2c7e549e9b5359f37efa2535e4622796228e
vikbhas <waraa.vignesh@gmail.com>
2020-12-07 19:27:35 +0530

    Feat: Valyris Translation

---
+++ app.js
@@ -0,0 +1,32 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?text=" + input
+}
+
+function errorHandler(error) {
+    console.log("ERROR", error);
+}
+
+
+function clickHandler() {
+    // console.log(txtInput)
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
